3d25b9299d84b018f2e57096704dfc0ef92062a6
kacey <kacey@voxel51.com>
2023-02-18 19:56:07 -0800

    hello

--- dummy_dataset.py
+++ dummy_dataset.py
@@ -1,6 +1,10 @@
-import fiftyone as fo
-import fiftyone.zoo.datasets.base as fozb
+#import fiftyone as fo
+#import fiftyone.zoo.datasets.base as fozb
 
+
+def hello():
+    print('hello')
+    
 def download_and_prepare(dataset_dir, scratch_dir, split=None):
     zoo_dataset = fozb.QuickstartDataset()
     dataset_type, _, _ = zoo_dataset._download_and_prepare(
